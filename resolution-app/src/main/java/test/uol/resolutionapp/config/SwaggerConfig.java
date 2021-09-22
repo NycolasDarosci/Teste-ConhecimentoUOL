8451e61f7927baf688a554a53ac9c8d8f534ea1e
Nycolas Alberto Darosci <nycolas.darosci@pampa.compasso>
2021-09-22 11:01:40 -0300

    config swagger

--- resolution-app/src/main/java/test/uol/resolutionapp/config/SwaggerConfig.java
+++ resolution-app/src/main/java/test/uol/resolutionapp/config/SwaggerConfig.java
@@ -1,10 +1,15 @@
 package test.uol.resolutionapp.config;
 
+import java.util.ArrayList;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,7 +24,16 @@ public class SwaggerConfig {
         .select()
         .apis(RequestHandlerSelectors.any())
         .paths(PathSelectors.any())
-        .build();
+        .build()
+        .apiInfo(metaInfo());
+    }
+
+    private ApiInfo metaInfo(){
+        ApiInfo apiInfo = new ApiInfo("Rest API", "Rest API contendo cinco endpoints: GET, GET-ID, POST, PUT, DELETE", "v1.0", "Terms",
+                new Contact("Nycolas Darosci","https://github.com/NycolasDarosci", null), null, null,
+                new ArrayList<VendorExtension>()
+                );
+        return apiInfo;
     }
 }
 
